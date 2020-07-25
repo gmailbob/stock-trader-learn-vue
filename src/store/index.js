14f4d54ef7b9281c9350aac83c9857a56ab446d5
gmailbob <mianyangbob@gmail.com>
2020-07-25 11:06:05 -0400

    add db.json for json-server and stocks in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+const { stocks } = require('../../db')
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    stocks: []
+  },
+  getters: {
+    stocks(state) {
+      return state.stocks;
+    }
   },
   mutations: {
+    setStocks(state, stocks) {
+      state.stocks = stocks;
+    },
+    // randStocks(state) { }
   },
   actions: {
+    // buyStock({ commit }, order) {
+    //   commit();
+    // },
+    initStocks({ commit }) {
+      commit('setStocks', stocks);
+    },
+    randomizeStocks({ commit }) {
+      commit('randStocks');
+    }
   },
   modules: {
   }
