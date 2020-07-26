1acc9cd5f745a0c8a95d9c74ba97d7a459b6c2be
gmailbob <mianyangbob@gmail.com>
2020-07-25 22:17:51 -0400

    store setup

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    stocks: []
+    stocks: [],
+    portfolio: {
+      funds: 10000,
+      records: []
+    }
   },
   getters: {
     stocks(state) {
       return state.stocks;
+    },
+    funds(state) {
+      return state.portfolio.funds;
+    },
+    stockPortfolio(state) {
+      return state.portfolio.records;
     }
   },
   mutations: {
@@ -19,17 +29,47 @@ export default new Vuex.Store({
       state.stocks = stocks;
     },
     // randStocks(state) { }
+    buyStock(state, order) {
+      const { stockId, stockPrice, quantity } = order;
+      if (state.portfolio.funds < stockPrice * quantity) return;
+      let record = state.portfolio.records.find(r => r.id == stockId);
+      if (!record) {
+        record = {
+          ...state.stocks.find(s => s.id == stockId),
+          quantity: 0
+        }
+        state.portfolio.records.push(record);
+      }
+      record.quantity += quantity;
+      state.portfolio.funds -= stockPrice * quantity;
+    },
+    sellStock(state, order) {
+      const { stockId, stockPrice, quantity } = order;
+      const i = state.portfolio.records.findIndex(r => r.id == stockId);
+      const record = state.portfolio.records[i];
+      if (record.quantity > quantity) {
+        record.quantity -= quantity;
+        state.portfolio.funds += stockPrice * quantity;
+      }
+      else {
+        state.portfolio.records.splice(i, 1);
+        state.portfolio.funds += stockPrice * record.quantity;
+      }
+    }
   },
   actions: {
-    // buyStock({ commit }, order) {
-    //   commit();
-    // },
     initStocks({ commit }) {
       commit('setStocks', stocks);
     },
     randomizeStocks({ commit }) {
       commit('randStocks');
-    }
+    },
+    buyStock({ commit }, order) {
+      commit('buyStock', order);
+    },
+    sellStock({ commit }, order) {
+      commit('sellStock', order);
+    },
   },
   modules: {
   }
